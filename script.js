cea44754e29a6ea8f0206272152f1714f572ba57
Halytus <158366688+Halytus@users.noreply.github.com>
2024-04-20 22:00:36 -0400

    Update script.js
    Updated script.js to match what I have hashed out so far.
    
    Still need to fix chngChart function to toggle Graph2's data source and reload chart

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var TITLE = 'Air Transport, Passengers Carried (1970–2018)';
+var TITLE = 'Grand Exchange Price of a Red Party Hat 2008-2023';
 
 // Caption underneath the chart
-var CAPTION = 'Source: The World Bank';
+var CAPTION = 'Source: Jagex and Kaggle';
 
 // Add download link to the caption
 CAPTION += '<br><a href="https://github.com/HandsOnDataViz/highcharts-line-annotated" style="color:blue">View data and code</a>, \
@@ -9,10 +9,10 @@ created by <a href="https://handsondataviz.org/" style="color: blue">HandsOnData
 with <a href="https://www.highcharts.com/" style="color: blue">Highcharts</a>';
 
 // x-axis label and label in tooltip
-var X_AXIS = 'Year';
+var X_AXIS = 'Date';
 
 // y-axis label and label in tooltip
-var Y_AXIS = 'Passengers';
+var Y_AXIS = 'Price in Gold Pieces(gp)';
 
 // Should y-axis start from 0? `true` or `false`
 var BEGIN_AT_ZERO = true;
@@ -20,11 +20,40 @@ var BEGIN_AT_ZERO = true;
 // `true` to show the legend, `false` to hide
 var SHOW_LEGEND = true;
 
+var TITLE2 = 'Grand Exchange Price of an Abyss Whip 2008-2023';
+
+// Caption underneath the chart
+var CAPTION2 = 'Source: Jagex and Kaggle';
+
+// Add download link to the caption
+CAPTION2 += '<br><a href="https://github.com/HandsOnDataViz/highcharts-line-annotated" style="color:blue">View data and code</a>, \
+created by <a href="https://handsondataviz.org/" style="color: blue">HandsOnDataViz</a> \
+with <a href="https://www.highcharts.com/" style="color: blue">Highcharts</a>';
+
+// x-axis label and label in tooltip
+var X_AXIS2 = 'Date';
+
+// y-axis label and label in tooltip
+var Y_AXIS2 = 'Price in Gold Pieces(gp)';
+
+// Should y-axis start from 0? `true` or `false`
+var BEGIN_AT_ZERO2 = true;
+
+// `true` to show the legend, `false` to hide
+var SHOW_LEGEND2 = true;
+
+var redph = './redpartyhat.csv';
+
+var abysswhip = './abysswhip.csv';
+
+var veracsflail = './veracsflail.csv';
+
+var graph2 = abysswhip;
 
 $(document).ready(function() {
 
   // Read data file and create a chart
-  $.get('./data.csv', function(csvString) {
+  $.get(redph, function(csvString) {
 
     var data = Papa.parse(csvString).data;
     var nSeries = data[0].length - 2
@@ -53,7 +82,7 @@ $(document).ready(function() {
     }
 
     // Now create the chart
-    Highcharts.chart('container', {
+    Highcharts.chart('graph', {
 
       chart: {
         type: 'line',
@@ -61,7 +90,7 @@ $(document).ready(function() {
         panning: true,
         panKey: 'shift',
         scrollablePlotArea: {
-            minWidth: 600
+            minWidth: 400
         }
       },
 
@@ -97,4 +126,86 @@ $(document).ready(function() {
 
   });
 
+
+
+
+  // Read data file and create a chart
+  $.get(graph2, function(csvString) {
+
+    var data = Papa.parse(csvString).data;
+    var nSeries = data[0].length - 2
+
+    var annotationPoints = data
+      .filter(function(x) {return x[nSeries+1] !== ""})
+      .map(function(x) {
+        return {
+          point: {
+            xAxis: 0,
+            yAxis: 0,
+            x: x[0],
+            y: x[1]
+          },
+          text: x[nSeries+1]
+        }
+      })
+
+    var series = []
+    for (var i = 1; i <= nSeries; i++) {
+      series.push({
+        data: data.map(function(x) { return [parseFloat(x[0]), parseFloat(x[i])] }),
+        name: data[0][i],
+        marker: { enabled: false },
+      })
+    }
+
+    // Now create the chart
+    Highcharts.chart('graph2', {
+
+      chart: {
+        type: 'line',
+        zoomType: 'x',
+        panning: true,
+        panKey: 'shift',
+        scrollablePlotArea: {
+            minWidth: 400
+        }
+      },
+
+      title: { text: TITLE2 },
+      caption: { text: CAPTION2 },
+      credits: { enabled: false },
+
+      annotations: [{
+        labelOptions: {
+          backgroundColor: 'rgba(255,255,255,0.8)',
+          verticalAlign: 'top',
+          y: 10
+        },
+        labels: annotationPoints
+      }],
+
+      xAxis: {
+        title: { text: X_AXIS2 },
+      },
+
+      yAxis: {
+        startOnTick: true,
+        min: BEGIN_AT_ZERO2 ? 0 : null,
+        title: { text: Y_AXIS2 },
+        labels: { formatter: function(x) {return x.value.toLocaleString()} }
+      },
+
+      legend: { enabled: SHOW_LEGEND2 },
+
+      series: series,
+
+    });
+
+  });
+
 });
+
+function chngChart(para) {
+  const elem = document.getElementById("graph2");
+  var graph2 = "veracsflail";
+};
